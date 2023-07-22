6aa455b3752aa635e82858536634d40810cdf37b
Karanpal0001 <128946946+Karanpal0001@users.noreply.github.com>
2023-07-22 15:40:16 +0530

    portfolio

---
+++ java/java.js
@@ -0,0 +1,15 @@
+function ok(){
+    window.location.href="ok2.html"
+
+    a=document.getElementById("hr6").value
+    sessionStorage.setItem("d1",a)
+
+    b=document.getElementById("hr7").value
+    sessionStorage.setItem("d2",b)
+
+    c=document.getElementById("hr8").value
+    sessionStorage.setItem("d3",c)
+
+    d=document.getElementById("hr9").value
+    sessionStorage.setItem("d4",d)
+}
